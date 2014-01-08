8bba15eb5aa4281ec6e10be8ef6c0bf2d374124f
Daniel Narvaez <dwnarvaez@gmail.com>
2014-01-08 02:18:40 +0100

    js-beautify

--- app.js
+++ app.js
@@ -18,14 +18,15 @@ function setupOutDir() {
         fs.mkdirSync("./out/rpmbuild");
         fs.mkdirSync("./out/rpmbuild/SOURCES");
         fs.mkdirSync("./out/rpmbuild/SPECS");
-    } catch(err) {
-    }
+    } catch (err) {}
 }
 
 function createSpec(module, callback) {
     var sourcePath = path.join('specs', module.name + '.spec');
 
-    fs.readFile(sourcePath, {encoding: 'utf8'}, function (error, data) {
+    fs.readFile(sourcePath, {
+        encoding: 'utf8'
+    }, function (error, data) {
         data = data.replace("@version@", module.version);
         data = data.replace("@shortcommit@", module.commit);
         data = data.replace("@release_date@", module.releaseDate);
@@ -39,44 +40,44 @@ function createSpec(module, callback) {
 
 function downloadSource(module, callback) {
     var command = 'spectool -g ' +
-                  '-C ' + path.join('out', 'rpmbuild', 'SOURCES') +
-                  ' ' + module.specPath;
+        '-C ' + path.join('out', 'rpmbuild', 'SOURCES') +
+        ' ' + module.specPath;
 
-    child_process.exec(command, function(error, stdout, stderr) {
+    child_process.exec(command, function (error, stdout, stderr) {
         callback(null);
     });
 }
 
 function buildSRPM(module, callback) {
     var command = 'rpmbuild' +
-                  ' -bs ' + module.specPath +
-                  ' -D \'_topdir ' + './out/rpmbuild\'';
+        ' -bs ' + module.specPath +
+        ' -D \'_topdir ' + './out/rpmbuild\'';
 
-    child_process.exec(command, function(error, stdout, stderr) {
+    child_process.exec(command, function (error, stdout, stderr) {
         callback(null);
     });
 }
 
 function getSRPMUrl(module) {
     var rpmPath = path.join('rpmbuild', 'SRPMS', module.name +
-                            '-' + module.version +
-                            '-' + module.releaseNumber +
-                            '.' + module.releaseDate +
-                            'git' + module.commit +
-                            '.fc20.src.rpm');
+        '-' + module.version +
+        '-' + module.releaseNumber +
+        '.' + module.releaseDate +
+        'git' + module.commit +
+        '.fc20.src.rpm');
 
     return 'http://jita.sugarlabs.org:3000/out/' + rpmPath;
 }
 
 function startMockBuild(module, rootName, host, callback) {
     var command = 'python scripts/mockremote.py' +
-                  ' -b ' + host +
-                  ' -r ' + rootName +
-                  ' --destdir ' + './out' +
-                  ' -a http://jita.sugarlabs.org:3000/out/' +
-                  ' ' + getSRPMUrl(module);
+        ' -b ' + host +
+        ' -r ' + rootName +
+        ' --destdir ' + './out' +
+        ' -a http://jita.sugarlabs.org:3000/out/' +
+        ' ' + getSRPMUrl(module);
 
-    child_process.exec(command, function(error, stdout, stderr) {
+    child_process.exec(command, function (error, stdout, stderr) {
         if (callback) {
             callback(null);
         }
@@ -85,13 +86,17 @@ function startMockBuild(module, rootName, host, callback) {
 
 function startCoprBuild(module, callback) {
     var apiUrl = 'http://copr-fe.cloud.fedoraproject.org' +
-                 '/api/coprs/dnarvaez/sugar/new_build/';
+        '/api/coprs/dnarvaez/sugar/new_build/';
 
-    var options = {'data': {'pkgs': getSRPMUrl(module)},
-                   'username': config.username,
-                   'password': config.password};
+    var options = {
+        'data': {
+            'pkgs': getSRPMUrl(module)
+        },
+        'username': config.username,
+        'password': config.password
+    };
 
-    rest.post(apiUrl, options).on('complete', function(data, response) {
+    rest.post(apiUrl, options).on('complete', function (data, response) {
         if (callback) {
             callback(null);
         }
@@ -100,8 +105,8 @@ function startCoprBuild(module, callback) {
 
 function fetchVersion(module, callback) {
     var url = 'https://raw.github.com/sugarlabs/' + module.name +
-              '/' + module.commit + '/configure.ac';
-    rest.get(url).on('complete', function(data, response) {
+        '/' + module.commit + '/configure.ac';
+    rest.get(url).on('complete', function (data, response) {
         callback(null, /AC_INIT\(\[[^\]]+],\[([^\]]+)/g.exec(data)[1]);
     });
 }
@@ -109,7 +114,9 @@ function fetchVersion(module, callback) {
 function getReleaseNumber(module, callback) {
     var jsonPath = 'releases.json';
 
-    fs.readFile(jsonPath, {encoding: 'utf8'}, function (error, data) {
+    fs.readFile(jsonPath, {
+        encoding: 'utf8'
+    }, function (error, data) {
         var releases = {};
 
         if (!error) {
@@ -117,7 +124,7 @@ function getReleaseNumber(module, callback) {
         }
 
         if (releases[module.name] === undefined) {
-            releases[module.name] = 1; 
+            releases[module.name] = 1;
         } else {
             releases[module.name]++;
         }
@@ -128,14 +135,13 @@ function getReleaseNumber(module, callback) {
             callback(null, releases[module.name]);
         });
     });
-} 
+}
 
 function startArmBuilds(module) {
     var host = "bender.sugarlabs.org:2222";
     startMockBuild(module, "fedora-18-armhfp", host, function (error) {
         startMockBuild(module, "fedora-19-armhfp", host, function (error) {
-            startMockBuild(module, "fedora-20-armhfp", host, function (error) {
-            });
+            startMockBuild(module, "fedora-20-armhfp", host, function (error) {});
         });
     });
 }
@@ -147,12 +153,12 @@ function buildModule(name, commit) {
     module.commit = commit;
     module.releaseDate = moment().format("YYYYMMDD");
     module.specPath = path.join('out', 'rpmbuild', "SPECS",
-                                module.name + '.spec');
+        module.name + '.spec');
 
-    fetchVersion(module, function(error, version) {
-        module.version = version; 
+    fetchVersion(module, function (error, version) {
+        module.version = version;
 
-        getReleaseNumber(module, function(error, releaseNumber) {
+        getReleaseNumber(module, function (error, releaseNumber) {
             module.releaseNumber = releaseNumber;
 
             createSpec(module, function (error) {
@@ -167,10 +173,10 @@ function buildModule(name, commit) {
     });
 }
 
-app.post('/api/github', function (request, response){
+app.post('/api/github', function (request, response) {
     var payload = JSON.parse(request.body.payload);
     buildModule(payload.repository.name,
-                payload.head_commit.id.slice(0, 7));
+        payload.head_commit.id.slice(0, 7));
     response.send(200);
 });
 
@@ -183,17 +189,17 @@ app.get('/repo/:name', function (request, response) {
     var name = request.params.name;
     var baseUrl = "http://copr.fedoraproject.org/results/dnarvaez/sugar/";
 
-    if(name.indexOf("armhfp") > -1) {
+    if (name.indexOf("armhfp") > -1) {
         baseUrl = "http://jita.sugarlabs.org:3000/out/";
     }
 
     response.send("[sugar]\n" +
-                  "name=Sugar\n" +
-                  "description=Sugar Learning Platform\n" +
-                  "baseurl=" + baseUrl + name + "\n" +
-                  "skip_if_unavailable=True\n" +
-                  "gpgcheck=0\n" +
-                  "enabled=1\n");
+        "name=Sugar\n" +
+        "description=Sugar Learning Platform\n" +
+        "baseurl=" + baseUrl + name + "\n" +
+        "skip_if_unavailable=True\n" +
+        "gpgcheck=0\n" +
+        "enabled=1\n");
 });
 
 setupOutDir();
